afa85fdadd0e8b49ad1cb8a56baf0405db1bb83e
devbatra2004 <dbatra767@gmail.com>
2025-08-31 18:09:58 +0530

    final

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     // to check whether the entered number is palindrome or not?
     int n;
-    cout<<"enter n : ";
-    cin>>n;
-   int original;
-    original=n;
-    int reversed=0;
-  while(n>0){
-    int digit=n%10;
-    reversed=reversed*10+digit;
-    n=n/10;
-  
-  }
-if(reversed==original){
-    cout<<"the number is palindrome";
-}
-else{
-    cout<<"the number is not palindrome";
-}
+    cout << "enter n : ";
+    cin >> n;
+    int original;
+    original = n;
+    int reversed = 0;
+    while (n > 0)
+    {
+        int digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n = n / 10;
+    }
+    if (reversed == original)
+    {
+        cout << "the number is palindrome";
+    }
+    else
+    {
+        cout << "the number is not palindrome";
+    }
     return 0;
 }
